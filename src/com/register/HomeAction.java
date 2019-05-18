13a1088bd1a00d9066377f862d19e81c874d6e67
Moshiur Rahman <moshiur.siyam@gmail.com>
2019-05-19 01:03:30 +0600

    attempt to try wild card

--- src/com/register/HomeAction.java
+++ src/com/register/HomeAction.java
@@ -13,19 +13,20 @@ import com.register.DBConnection;
 public class HomeAction {
 
 	public List<List<String>> userData;
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public List<List<String>> getUserData() {
 		return userData;
 	}
 	
-	public String getName() {
-		return "siyam";
-	}
-
-//	public void setUser(List<List<String>> userData) {
-//		this.userData = userData;
-//	}
-
 	public String launchRegister() {
 		System.out.println("Entering into launchRegister of registerAction");
 		System.out.println("Exiting from launchRegister of registerAction");
@@ -38,7 +39,8 @@ public class HomeAction {
 		return "lunchDataTable";
 	}
 
-	public String getData() throws SQLException {
+	public String readDataFromDb() throws SQLException {
+		System.out.println("Entering readDataFromDb");
 		ResultSet result = DBConnection.oracleConnection().createStatement().executeQuery("SELECT * FROM students");
 		List info = new ArrayList<ArrayList<String>>();
 		while (result.next()) {
@@ -48,6 +50,23 @@ public class HomeAction {
 			prop.add(result.getString("last_name"));
 			info.add(prop);
 		}
+		userData = info;
+		DBConnection.oracleConnection().close();
+		System.out.println("Exiting readDataFromDb");
+		return Action.SUCCESS;
+	}
+	
+	public String readUserInfo() throws SQLException{
+		ResultSet result = DBConnection.oracleConnection().createStatement().executeQuery("SELECT * FROM students WHERE first_name = '" + id +"'");
+		List info = new ArrayList<ArrayList<String>>();
+		while (result.next()) {
+			System.out.println(result.getString("first_name"));
+			List prop = new ArrayList<String>();
+			prop.add(result.getString("first_name"));
+			prop.add(result.getString("last_name"));
+			info.add(prop);
+		}
+		
 		userData = info;
 		DBConnection.oracleConnection().close();
 		return Action.SUCCESS;
